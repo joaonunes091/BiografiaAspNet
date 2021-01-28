ec002cf3a4b04d4a0608e41f93349272c0002c6e
Joao Nunes <jpgn-1@hotmail.com>
2021-01-28 18:09:21 +0000

    Vistas Editar Sucesso e Inexistente

--- BiografiaAspNet/Controllers/DadosPessoaisController.cs
+++ BiografiaAspNet/Controllers/DadosPessoaisController.cs
@@ -1,6 +1,8 @@
 ﻿using BiografiaAspNet.Data;
 using BiografiaAspNet.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,7 +21,7 @@ namespace BiografiaAspNet.Controllers
 
         public IActionResult Index()
         {
-            IEnumerable <DadosPessoais> dadosPessoais = _db.DadosPessoais;
+            IEnumerable<DadosPessoais> dadosPessoais = _db.DadosPessoais;
             return View(dadosPessoais);
         }
 
@@ -32,11 +34,58 @@ namespace BiografiaAspNet.Controllers
         //POST - Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Nome,DataNascimento,Naturalidade,Nacionalidade")]DadosPessoais dadosPessoais)
+        public async Task<IActionResult> Create([Bind("Id,Nome,DataNascimento,Naturalidade,Nacionalidade")] DadosPessoais dadosPessoais)
         {
             _db.DadosPessoais.Add(dadosPessoais);
             await _db.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
+
+        // GET: Edit
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dadosPessoais = await _db.DadosPessoais.FindAsync(id);
+
+            if (dadosPessoais == null)
+            {
+                return View("Inexistente");
+            }
+
+            return View(dadosPessoais);
+        }
+
+        // POST: Produtos/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,DataNascimento,Naturalidade,Nacionalidade")] DadosPessoais dadosPessoais)
+        {
+            if (id != dadosPessoais.Id)
+            {
+                return NotFound();
+            }
+            try
+            {
+                _db.Update(dadosPessoais);
+                await _db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+
+                ModelState.AddModelError("", "Ocorreu um erro. Não foi possível guardar o produto. Tente novamente e se o problema persistir contacte a assistência.");
+                return View(dadosPessoais);
+
+            }
+
+            ViewBag.Mensagem = "Produto alterado com sucesso";
+            return View("Sucesso");
+        }
+
+
+
     }
 }
